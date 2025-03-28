4f20da5b7fbb7a59e8fe05b5052b53935561bbd4
Fredrich Tan <cloudtemplar26@gmail.com>
2025-03-28 05:39:22 -0400

    validation fixed with additional function as showing a warning that name and descriptions fields cannot be empty

--- Assign2_Tan_Fredrich_101318950/Assign2_Tan_Fredrich_101318950/AddProductViewController.swift
+++ Assign2_Tan_Fredrich_101318950/Assign2_Tan_Fredrich_101318950/AddProductViewController.swift
@@ -19,21 +19,32 @@ class AddProductViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    func showAlert(message: String) {
+            let alert = UIAlertController(title: "Invalid Input",
+                                          message: message,
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true, completion: nil)
+        }
     
     @IBAction func saveTapped(_ sender: UIButton){
         
-        guard let nameText = nameField.text, !nameText.isEmpty
-              let descriptionText = descriptionTextField.text, !descriptionTextField.isEmpty else{
-            showAlert(message"Name and desxription cannot be empty.")
-            return
-        }
+            guard
+                    let nameText = nameField.text, !nameText.isEmpty,
+                    let descText = descriptionTextField.text, !descText.isEmpty
+                else {
+                    showAlert(message: "Name and description cannot be empty.")
+                    return
+                }
+       
+
         
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
         let product = Product(context: context)
         product.id = UUID()
-        product.name = nameField.text
-        product.productDescription = descriptionTextField.text
+        product.name = nameText
+        product.productDescription = descText
         product.provider  = providerField.text
         product.price = Double(priceField.text ?? "0") ?? 0.0
         
