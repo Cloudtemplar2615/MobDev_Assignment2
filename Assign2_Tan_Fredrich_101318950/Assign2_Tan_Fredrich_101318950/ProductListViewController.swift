384d0d8519cadeac84bed8c8c104901803b6318f
Fredrich Tan <cloudtemplar26@gmail.com>
2025-03-28 04:31:47 -0400

    Filled ProductListViewController  with a a UITABLEVIEW fetch product to store data for product used prototype cell

--- Assign2_Tan_Fredrich_101318950/Assign2_Tan_Fredrich_101318950/ProductListViewController.swift
+++ Assign2_Tan_Fredrich_101318950/Assign2_Tan_Fredrich_101318950/ProductListViewController.swift
@@ -5,3 +5,46 @@
 //  Created by Fredrich Tan on 2025-03-28.
 //
 
+import UIKit
+import CoreData
+
+class ProductListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+   
+    @IBOutlet weak var tableView: UITableView!
+    var products: [Product] = []
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchProducts()
+    }
+   
+    func fetchProducts(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request: NSFetchRequest<Product> = Product.fetchRequest()
+       
+        do{
+            products = try context.fetch(request)
+            tableView.reloadData()
+        }catch{
+            print("Failed to fetch products: \(error)")
+        }
+    }
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        let product = products[indexPath.row]
+        cell.textLabel?.text = product.name
+        return cell
+    }
+
+
+}
+
+
